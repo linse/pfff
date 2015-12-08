ea3a2a5e9d0fc908ee002dd8622c5d827c3d1b1c
sschirmer <sschirmer@etsy.com>
2015-12-08 21:40:48 +0000

    Add phan tests.

---
+++ tests/php/parsing/phan0013_assignment_types.php
@@ -0,0 +1,28 @@
+<?php
+
+class A {
+    // @var int
+    const ALPHA = 42;
+
+    public function str() : string {
+        return 'string';
+    }
+
+    public function instance() : A {
+        return $this;
+    }
+}
+
+class B {
+    private $a;
+
+    public function __construct() {
+        $str = 'string';
+
+        $alpha = A::$alpha;
+
+        $a = new A;
+        $str = $a->str();
+        $this->a = $a->instance();
+    }
+}
